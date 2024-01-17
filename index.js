a001474265bdc9acbd217dad31ca108e4c187113
MaslowCorporation <maslow.gpt.corp.97200@gmail.com>
2024-01-17 01:59:36 +0100

    First commit ;-)

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+
+/**
+ * 
+ * @param {*} answers, an object containing 
+ * as keys the outputs of template_prompts.json.
+ * 
+ * @param {*} appRootPath, AKA process.cwd(), the path of the Terminal when
+ * the command is running
+ * 
+ * @param {*} generatedFolderPath, the path of the folder created from the template,
+ * if any such got created
+ * 
+ * @return ....
+ * 
+ * Do your own thing, bruddha ;-)
+ */
+export async function main(
+    answers,
+    appRootPath,
+    generatedFolderPath
+) {
+    console.log("Hello ! from the main function!");
+
+}
